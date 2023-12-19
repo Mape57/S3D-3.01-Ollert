c236040b324116aa91c8f08d40b8c3f8b3889514
GregoireHirtz <hirtz.gregoire@ghmail.fr>
2023-12-19 11:06:19 +0100

    correction Sauvegarde + Test

--- Sources/src/ollert/Sauvegarde.java
+++ Sources/src/ollert/Sauvegarde.java
@@ -4,9 +4,11 @@ import java.io.*;
 
 public class Sauvegarde {
 
+    public static String DIR="ressource/fichier/";
+
     public static Page chargerPage(String nomFichier) {
         try{
-            File fichier =  new File(nomFichier);
+            File fichier =  new File(DIR+nomFichier);
             ObjectInputStream ois =  new ObjectInputStream(new FileInputStream(fichier));
             Page p = (Page)ois.readObject();
             ois.close();
@@ -18,7 +20,8 @@ public class Sauvegarde {
 
     public static boolean sauvegarderPage(Page page) {
         try{
-            File fichier =  new File("ressource/"+page.getTitre()+".ol");
+            File fichier =  new File(DIR+page.getTitre()+".ol");
+            //System.out.println(fichier.getAbsolutePath());
             // ouverture d'un flux sur un fichier
             ObjectOutputStream oos =  new ObjectOutputStream(new FileOutputStream(fichier)) ;
             // sérialization de la Page
@@ -27,6 +30,7 @@ public class Sauvegarde {
             oos.close();
             return true;
         }catch (IOException e){
+            System.out.println(e.getMessage());
             return false;
         }
     }
--- Sources/test/ollert/SauvegardeTest.java
+++ Sources/test/ollert/SauvegardeTest.java
@@ -23,29 +23,20 @@ public class SauvegardeTest {
         page3.obtenirListe(0).creerTache("Tache 01");
         page3.obtenirListe(0).creerTache("Tache 02");
         page3.obtenirListe(1).creerTache("Tache 11");
-
-
-        File dossier = new File("ressource");
-        if (dossier.exists() && dossier.isDirectory()) {
-            // Liste des fichiers dans le dossier
-            File[] fichiers = dossier.listFiles();
-            if (fichiers != null) {
-                for (File fichier : fichiers) {
-                    fichier.delete();
-                }
-            }
-        }
     }
 
-    @AfterAll
-    public static void close(){
-        File dossier = new File("ressource");
+    @AfterEach
+    public void afterUp(){
+        File dossier = new File(Sauvegarde.DIR);
         if (dossier.exists() && dossier.isDirectory()) {
             // Liste des fichiers dans le dossier
             File[] fichiers = dossier.listFiles();
             if (fichiers != null) {
                 for (File fichier : fichiers) {
-                    fichier.delete();
+
+                    // si fichier .ol => suppression
+                    if (fichier.getName().endsWith(".ol"))
+                        fichier.delete();
                 }
             }
         }
@@ -55,16 +46,12 @@ public class SauvegardeTest {
     public void test_sauvegarderPage(){
         // sauvegarde valide
         assertTrue(Sauvegarde.sauvegarderPage(page1));
-        File fichier1 = new File("ressource/Ollert1.ol");
+        File fichier1 = new File(Sauvegarde.DIR+"Ollert1.ol");
         assertTrue(fichier1.exists());
 
         // verifications fichier non présent (normal)
-        File fichier2 = new File("ressource/Ollert2.ol");
+        File fichier2 = new File(Sauvegarde.DIR+"Ollert2.ol");
         assertFalse(fichier2.exists());
-
-        // verification non présence fichier mauvaise place
-        File fichier3 = new File("Ollert1.ol");
-        assertFalse(fichier3.exists());
     }
 
     @Test
@@ -75,18 +62,18 @@ public class SauvegardeTest {
 
         // fichier valide
 
-        Page page1Chargee = Sauvegarde.chargerPage("ressource/Ollert1.ol");
+        Page page1Chargee = Sauvegarde.chargerPage("Ollert1.ol");
         assertEquals(page1.toString(), page1Chargee.toString());
 
-        Page page2Chargee = Sauvegarde.chargerPage("ressource/Ollert2.ol");
+        Page page2Chargee = Sauvegarde.chargerPage("Ollert2.ol");
         assertEquals(page2.toString(), page2Chargee.toString());
 
-        Page page3Chargee = Sauvegarde.chargerPage("ressource/Ollert3.ol");
+        Page page3Chargee = Sauvegarde.chargerPage("Ollert3.ol");
         assertEquals(page3.toString(), page3Chargee.toString());
 
 
         // fichier non valide => null
-        Page page4Chargee = Sauvegarde.chargerPage("ressource/Ollert4.ol");
+        Page page4Chargee = Sauvegarde.chargerPage("Ollert4.ol");
         assertNull(page4Chargee);
     }
 }
