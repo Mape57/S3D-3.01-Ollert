e17def2103f8d8239bcdbde3c0f4bc3ad3c5328e
GregoireHirtz <hirtz.gregoire@ghmail.fr>
2023-12-20 17:07:14 +0100

    correction src dans doc

--- Documents/Iteration2/UML/src/mvc/vue/tache/VueTache.java
+++
@@ -1,8 +0,0 @@
-package mvc.vue.tache;
-
-import mvc.vue.VuePrincipale;
-import ollert.tache.TachePrincipale;
-
-public interface VueTache extends VuePrincipale {
-	TachePrincipale getTache();
-}
--- Documents/Iteration2/UML/src/ollert/Parent.java
+++
@@ -1,4 +0,0 @@
-package ollert;
-
-public interface Parent {
-}
