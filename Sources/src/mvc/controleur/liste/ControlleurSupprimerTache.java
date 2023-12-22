d98542d7cf87079f6b7ea8309c176a203899d7a0
GregoireHirtz <hirtz.gregoire@ghmail.fr>
2023-12-22 10:58:17 +0100

    boite de dialogue pour suppression listeTache

--- Sources/src/mvc/controleur/liste/ControlleurSupprimerTache.java
+++ Sources/src/mvc/controleur/liste/ControlleurSupprimerTache.java
@@ -2,9 +2,14 @@ package mvc.controleur.liste;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 import mvc.modele.ModeleOllert;
 import mvc.vue.liste.VueListe;
+
+import java.util.Optional;
 
 public class ControlleurSupprimerTache implements EventHandler<ActionEvent> {
 	private ModeleOllert modele;
@@ -15,8 +20,23 @@ public class ControlleurSupprimerTache implements EventHandler<ActionEvent> {
 
 	@Override
 	public void handle(ActionEvent event) {
-		Button btn = (Button) event.getSource();
-		VueListe vl = (VueListe) btn.getParent().getParent();
-		this.modele.removeListeTache(vl.getListe());
+		Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+		alert.setTitle("Confirmation de Suppression");
+		alert.setHeaderText(null);
+		alert.setContentText("Voulez-vous vraiment supprimer cet élément ?");
+
+		ButtonType buttonTypeValider = new ButtonType("Valider");
+		ButtonType buttonTypeAnnuler = new ButtonType("Annuler", ButtonBar.ButtonData.CANCEL_CLOSE);
+		alert.getButtonTypes().setAll(buttonTypeValider, buttonTypeAnnuler);
+
+		Optional<ButtonType> result = alert.showAndWait();
+
+		if (result.isPresent() && result.get() == buttonTypeValider) {
+			Button btn = (Button) event.getSource();
+			VueListe vl = (VueListe) btn.getParent().getParent();
+			this.modele.removeListeTache(vl.getListe());
+		} else {
+			System.out.println("Suppression annulée.");
+		}
 	}
 }
