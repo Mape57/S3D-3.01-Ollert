8d564551b7743ace321bba29b491bc4c4574361c
Stephane Loppinet <stephaneloppinet@gmail.com>
2023-12-22 10:31:38 +0100

    Suppression à la version stable sans le StackPane pour l'affichage provisoirement
    Took 15 minutes

--- Sources/src/mvc/vue/page/VuePageTableau.java
+++ Sources/src/mvc/vue/page/VuePageTableau.java
@@ -3,7 +3,6 @@ package mvc.vue.page;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import mvc.fabrique.FabriqueVueTableau;
 import mvc.modele.ModeleOllert;
 import mvc.modele.Sujet;
@@ -19,7 +18,7 @@ import java.util.List;
 /**
  * Classe de la vue représentant une page sous forme de tableau
  */
-public class VuePageTableau extends StackPane implements VuePage {
+public class VuePageTableau extends HBox implements VuePage {
 	/**
 	 * Liste des observateurs (les vues des listes de la page (VueListeTableau))
 	 */
@@ -30,22 +29,15 @@ public class VuePageTableau extends StackPane implements VuePage {
 	private Page page;
 
 	/**
-	 * HBox contenant les vues des listes de la page
-	 */
-	private HBox hBox;
-
-	/**
 	 * Constructeur de la classe VuePageTableau
 	 * @param page Page réelle que représente la vue
 	 */
 	public VuePageTableau(Page page, ModeleOllert modeleControle) {
 		this.observateurs = new ArrayList<>();
 		this.page = page;
-		this.hBox = new HBox();
-		this.hBox.setSpacing(10);
-		this.hBox.setPadding(new Insets(10));
-		this.hBox.setStyle("-fx-background-color: red;");
-		this.getChildren().add(this.hBox);
+		this.setSpacing(10);
+		this.setPadding(new Insets(10));
+		this.setStyle("-fx-background-color: red;");
 	}
 
 	/**
@@ -86,18 +78,18 @@ public class VuePageTableau extends StackPane implements VuePage {
 		for (int i = 0; i < this.page.sizeListe(); i++) {
 			ListeTaches l = this.page.getListeTaches(i);
 			// la taille ne correspond pas : creation d'une Vue Liste
-			if (i >= this.hBox.getChildren().size()) {
+			if (i >= this.getChildren().size()) {
 				VueListe vl_tmp = new FabriqueVueTableau().creerVueListe(l, modele);
-				this.hBox.getChildren().add(i, (Node) vl_tmp);
+				this.getChildren().add(i, (Node) vl_tmp);
 				vl_tmp.actualiser(modele);
 				continue;
 			}
 
-			VueListeTableau vl = (VueListeTableau) this.hBox.getChildren().get(i);
+			VueListeTableau vl = (VueListeTableau) this.getChildren().get(i);
 			// la Vue et la Liste ne correspondent pas : insertion d'une Vue Liste
 			if (!vl.getListe().equals(l)) {
 				VueListeTableau vl_tmp = new FabriqueVueTableau().creerVueListe(l, modele);
-				this.hBox.getChildren().add(i, vl_tmp);
+				this.getChildren().add(i, vl_tmp);
 				vl_tmp.actualiser(modele);
 				continue;
 			}
@@ -108,8 +100,8 @@ public class VuePageTableau extends StackPane implements VuePage {
 
 		// TODO a tester
 		// nombre de liste de la page < nombre de liste de la vue : suppression des vues en trop
-		if (this.hBox.getChildren().size() > this.page.sizeListe())
-			this.hBox.getChildren().remove(this.page.sizeListe(), this.hBox.getChildren().size());
+		if (this.getChildren().size() > this.page.sizeListe())
+			this.getChildren().remove(this.page.sizeListe(), this.getChildren().size());
 
 		this.notifierObservateurs();
 	}
