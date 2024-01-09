7f14c985e04bf1ad920875853d6fad5e252197d1
Stephane Loppinet <stephaneloppinet@gmail.com>
2024-01-09 14:00:30 +0100

    v1 affichage Gantt
    Took 1 hour 47 minutes
    
    Took 13 minutes

--- Sources/src/mvc/vue/page/VuePageGantt.java
+++ Sources/src/mvc/vue/page/VuePageGantt.java
@@ -1,25 +1,37 @@
 package mvc.vue.page;
 
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import mvc.controleur.liste.ControlleurModifierTitre;
+import mvc.controleur.page.ControlleurAjouterListe;
+import mvc.controleur.page.ControlleurTableau;
+import mvc.controleur.page.ControlleurTableur;
+import mvc.fabrique.FabriqueVueTableau;
 import mvc.modele.ModeleOllert;
 import mvc.modele.Sujet;
+import mvc.vue.Observateur;
+import mvc.vue.liste.VueListe;
+import mvc.vue.liste.VueListeTableau;
 import ollert.Page;
 import ollert.tache.Comparateur.ComparateurDateDebut;
 import ollert.tache.ListeTaches;
 import ollert.tache.TachePrincipale;
 
-import java.awt.*;
+import java.lang.reflect.Array;
 import java.time.LocalDate;
+import java.time.format.TextStyle;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
+import java.awt.Point;
 
 /**
  * Classe de la vue représentant une page sous forme de tableau
@@ -34,7 +46,7 @@ public class VuePageGantt extends HBox implements VuePage {
     private final int ORIGIN_X = 20;
     private final int ORIGIN_Y = 20;
     private final int HAUTEUR_TACHE = 30;
-    private final int LARGEUR_JOUR = 20;
+    private final int LARGEUR_JOUR = 40;
 
     /**
      * Constructeur de la classe VuePageTableau
@@ -65,31 +77,27 @@ public class VuePageGantt extends HBox implements VuePage {
         centre.getChildren().clear();
 
         Page page = (Page)modele.getDonnee();
-        List<ListeTaches> listes = page.getListes();
-
-
 
         // Dessin du diagramme de Gantt sur le Canvas
-        this.draw(canvas.getGraphicsContext2D(), listes);
+        ArrayList<TachePrincipale> tachesPrincipalesSansAntecents = this.getTachesPrincipalesSansAntecedentTriParDateDebut(page);
+        this.draw(canvas.getGraphicsContext2D(), tachesPrincipalesSansAntecents);
         // Ajout du Canvas à la HBox
         centre.getChildren().add(canvas);
 
     }
 
-    private void draw(GraphicsContext gc, List<ListeTaches> listes){
-
-
+    public ArrayList<TachePrincipale> getTachesPrincipalesSansAntecedentTriParDateDebut(Page page) {
+        List<ListeTaches> listes = page.getListes();
         // On rassemble les taches de toutes les listes triées par date de début
         ArrayList<TachePrincipale> taches = new ArrayList<>();
-        for(ListeTaches lt : listes){
-            for(TachePrincipale t : lt.getTaches()){
-                if(t.getDateDebut() != null && t.getDateFin() != null) {
-                    if(t.getAntecedents().isEmpty()) {
+        for (ListeTaches lt : listes) {
+            for (TachePrincipale t : lt.getTaches()) {
+                if (t.getDateDebut() != null && t.getDateFin() != null) {
+                    if (t.getAntecedents().isEmpty()) {
                         taches.add(t);
-                    }
-                    else{
+                    } else {
                         // Recherche de la date de fin du calendrier (la tâche qui termine le plus tard)
-                        if(this.dateFinCalendrier == null || t.getDateFin().isAfter(this.dateFinCalendrier))
+                        if (this.dateFinCalendrier == null || t.getDateFin().isAfter(this.dateFinCalendrier))
                             this.dateFinCalendrier = t.getDateFin();
                     }
                 }
@@ -102,6 +110,11 @@ public class VuePageGantt extends HBox implements VuePage {
         // Date de début de calendrier après tri des tâches qui ne dépendent d'aucune autre car elle sont logiquement avant celles qui dépendent de celles-ci
         this.dateDebutCalendrier = taches.get(0).getDateDebut();
 
+        return taches;
+    }
+
+    private void draw(GraphicsContext gc, ArrayList<TachePrincipale> taches){
+
         if(taches.isEmpty()){
             System.out.println("PAS DE TACHE AVEC UNE DATE DEBUT ET DE FIN");
             gc.setFill(Color.BLACK);
@@ -109,18 +122,14 @@ public class VuePageGantt extends HBox implements VuePage {
         }
         else {
             HashMap<TachePrincipale, Point> tacheCoordPourFleche = new HashMap<>();
-            LocalDate dateDebut = taches.get(0).getDateDebut();
-            int coordYPinceau = ORIGIN_Y;
+            int coordYPinceau = ORIGIN_Y + 60;
             int largeurTache;
             int hauteurTache = HAUTEUR_TACHE;
             int largeurJour = LARGEUR_JOUR;
 
             for (TachePrincipale tache : taches) {
-                int coordXPinceau = ORIGIN_X;
 
                 this.dessinerTitreTache(gc, tache.getTitre(), coordYPinceau, HAUTEUR_TACHE);
-                // Espace de 200 pixels pour commencer à dessiner les taches
-                coordXPinceau += 200;
 
                 // On calcule la largeur de la tache
                 largeurTache = tache.getDuree() * largeurJour;
@@ -128,24 +137,23 @@ public class VuePageGantt extends HBox implements VuePage {
                 Color randomColor = new Color(Math.random(), Math.random(), Math.random(), 1.0);
 
                 // On dessine la tâche
-                this.dessinerTache(gc, randomColor, coordXPinceau, coordYPinceau, largeurTache, tache.getDateDebut(), HAUTEUR_TACHE, LARGEUR_JOUR);
-                tacheCoordPourFleche.put(tache, new Point(coordXPinceau, coordYPinceau+HAUTEUR_TACHE/2));
+                int coordXTache = this.dessinerTache(gc, randomColor, coordYPinceau, largeurTache, tache.getDateDebut(), HAUTEUR_TACHE, LARGEUR_JOUR);
+                tacheCoordPourFleche.put(tache, new Point(coordXTache, coordYPinceau+HAUTEUR_TACHE/2));
 
                 while(!tache.getDependances().isEmpty()) {
                     for (TachePrincipale tacheDependante : tache.getDependances()) {
                         int largeurTacheAntecedent = largeurTache;
                         // Si la tache dépendante n'a pas déjà été dessinée, on dessine la tache et la fleche
                         if (!tacheCoordPourFleche.containsKey(tacheDependante)) {
-                            coordXPinceau += largeurTache;
                             coordYPinceau += hauteurTache + 20;
                             largeurTache = tacheDependante.getDuree() * largeurJour;
                             this.dessinerTitreTache(gc, tacheDependante.getTitre(), coordYPinceau, HAUTEUR_TACHE);
-                            this.dessinerTache(gc, randomColor, coordXPinceau, coordYPinceau, largeurTache, tacheDependante.getDateDebut(), HAUTEUR_TACHE, LARGEUR_JOUR);
-                            tacheCoordPourFleche.put(tacheDependante, new Point(coordXPinceau, coordYPinceau + HAUTEUR_TACHE / 2));
+                            coordXTache = this.dessinerTache(gc, randomColor, coordYPinceau, largeurTache, tacheDependante.getDateDebut(), HAUTEUR_TACHE, LARGEUR_JOUR);
+                            tacheCoordPourFleche.put(tacheDependante, new Point(coordXTache, coordYPinceau + HAUTEUR_TACHE / 2));
                         }
-                        /*Point pDepart = tacheCoordPourFleche.get(tache);
+                        Point pDepart = tacheCoordPourFleche.get(tache);
                         Point pArrivee = tacheCoordPourFleche.get(tacheDependante);
-                        this.dessinerFleche(gc, pDepart, pArrivee, largeurTacheAntecedent);*/
+                        this.dessinerFleche(gc, pDepart, pArrivee, largeurTacheAntecedent);
                     }
                     tache = tache.getDependances().get(0);
 
@@ -154,16 +162,22 @@ public class VuePageGantt extends HBox implements VuePage {
                 // On décale le pinceau
                 coordYPinceau += hauteurTache + 20;
             }
+            this.dessinerAxeDates(gc);
         }
     }
 
-    private void dessinerTache(GraphicsContext gc, Color color, int coordXPinceau, int coordYPinceau, int largeurTache, LocalDate dateDebut, int hauteurTache, int largeurJour){
+    private int dessinerTache(GraphicsContext gc, Color color, int coordYPinceau, int largeurTache, LocalDate dateDebut, int hauteurTache, int largeurJour){
+
+        // Espace de 200 pixels pour commencer à dessiner les taches
+        int coordXPinceau = ORIGIN_X + 200;
 
         long decalageJours = ChronoUnit.DAYS.between(dateDebutCalendrier, dateDebut);
         coordXPinceau += decalageJours * largeurJour;
 
         gc.setFill(color);
         gc.fillRect(coordXPinceau, coordYPinceau, largeurTache, hauteurTache);
+
+        return coordXPinceau;
     }
 
     private void dessinerTitreTache(GraphicsContext gc, String titre, int coordYPinceau, int hauteurTache){
@@ -189,6 +203,27 @@ public class VuePageGantt extends HBox implements VuePage {
         gc.setLineWidth(4);
         gc.strokeLine(pDepart.getX()+largeurTacheAntecedent, pDepart.getY(), pArrivee.getX(), pArrivee.getY());
     }
+
+    private void dessinerAxeDates(GraphicsContext gc){
+        int coordXPinceau = ORIGIN_X + 200; // décalage initial de 200 pixels
+        int coordYPinceau = ORIGIN_Y; // position de l'axe des dates
+        int largeurJour = LARGEUR_JOUR;
+        gc.setFill(Color.BLACK);
+        gc.setFont(new Font("Arial", 12));
+
+        // Parcourir chaque jour entre dateDebutCalendrier et dateFinCalendrier
+        for (LocalDate date = dateDebutCalendrier; !date.isAfter(dateFinCalendrier); date = date.plusDays(1)) {
+            // Dessiner une marque pour chaque jour
+            gc.fillText(String.valueOf(date.getDayOfMonth()), coordXPinceau, coordYPinceau);
+            // Dessiner le nom du mois au début de chaque mois
+            if (date.getDayOfMonth() == 1) {
+                gc.fillText(date.getMonth().getDisplayName(TextStyle.FULL, Locale.FRANCE), coordXPinceau, coordYPinceau - 20);
+            }
+            coordXPinceau += largeurJour;
+        }
+    }
+
+
 
     @Override
     public Node getChildrenPrincipale() {
