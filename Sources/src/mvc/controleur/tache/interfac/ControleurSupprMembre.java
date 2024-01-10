321135da495c085e55753974bc54090bfed96c0e
LeMouj <enzo.mougin9@etu.univ-lorraine.fr>
2024-01-11 00:10:12 +0100

    correction affichage suppresion membre et utilisateur plus ajout clarté

--- Sources/src/mvc/controleur/tache/interfac/ControleurSupprMembre.java
+++ Sources/src/mvc/controleur/tache/interfac/ControleurSupprMembre.java
@@ -2,9 +2,15 @@ package mvc.controleur.tache.interfac;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import mvc.modele.ModeleOllert;
 import ollert.tache.donneesTache.Utilisateur;
 
@@ -23,6 +29,7 @@ public class ControleurSupprMembre implements EventHandler<ActionEvent> {
     // FIXME : modeSuppression est-il une validation ?
     private Boolean modeSuppression;
     private List<HBox> selected;
+    private List<HBox> list;
 
     /**
      * Constructeur de la classe ControleurModification
@@ -30,19 +37,8 @@ public class ControleurSupprMembre implements EventHandler<ActionEvent> {
     public ControleurSupprMembre(ModeleOllert modele, List<HBox> l) {
         this.modele = modele;
         this.modeSuppression = false;
+        this.list = l;
         this.selected = new ArrayList<>();
-        for (HBox x : l){
-            x.setOnMouseClicked(e -> {
-                if (modeSuppression) {
-                    if (!selected.contains(x)){
-                        selected.add(x);
-                    }
-                    else {
-                        selected.remove(x);
-                    }
-                }
-            });
-        }
     }
 
     @Override
@@ -50,16 +46,45 @@ public class ControleurSupprMembre implements EventHandler<ActionEvent> {
         this.modeSuppression = !this.modeSuppression;
 
         if (!modeSuppression){
-            for (HBox h : selected){
+            for (HBox h : list){
+                h.getChildren().remove(2);
+            }
+            for (HBox h2 : selected){
                 String rec = "";
                 for (Utilisateur u : modele.getTacheEnGrand().getMembres()){
-                    if (u.getPseudo().contains(((Label)h.getChildren().get(1)).getText())){
+                    if (u.getPseudo().contains(((Label)h2.getChildren().get(1)).getText())){
                         rec = u.getPseudo();
                     }
                 }
                 modele.getTacheEnGrand().supprimerUtilisateur(rec);
+                h2.setDisable(true);
+                h2.setVisible(false);
             }
             selected = new ArrayList<>();
+        }
+        else {
+            for (HBox h : list){
+                Circle circle = new Circle(10);
+                circle.setFill(Color.GREEN);
+                Rectangle bar = new Rectangle(8, 2);
+                bar.setFill(Color.WHITE);
+                StackPane stackPane = new StackPane();
+                stackPane.getChildren().addAll(circle, bar);
+                stackPane.setOnMouseClicked(e -> {
+                    for (HBox x : this.list) {
+                        if (modeSuppression) {
+                            if (!selected.contains(x)) {
+                                ((Circle)stackPane.getChildren().get(0)).setFill(Color.RED);
+                                selected.add(x);
+                            } else {
+                                ((Circle)stackPane.getChildren().get(0)).setFill(Color.GREEN);
+                                selected.remove(x);
+                            }
+                        }
+                    }
+                });
+                h.getChildren().add(stackPane);
+            }
         }
         ((Button)actionEvent.getSource()).setText(modeSuppression?"Valider" : "Supprimer");
     }
